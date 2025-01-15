545fd2f4be302ab83a3618ecd7c95af388607f9a
Yamen Zakhour <yz.kh@icloud.com>
2025-01-15 08:48:55 +0400

    Fix: Routes

--- rua/hooks.py
+++ rua/hooks.py
@@ -239,7 +239,7 @@ app_license = "mit"
 # ]
 
 # Automatically update python controller files with type annotations for this app.
-# export_python_type_annotations = True
+# export_python_type_annotations = Trues
 
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
