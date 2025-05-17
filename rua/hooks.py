1fcff877b0e47aea53d9afe5d05f17ff32b5328a
Yamen Zakhour <yz.kh@icloud.com>
2025-05-17 12:30:31 +0400

    init

--- rua/hooks.py
+++ rua/hooks.py
@@ -263,3 +263,5 @@ scheduler_events = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
+website_route_rules = [{'from_route': '/dashboard/<path:app_path>', 'to_route': 'dashboard'},]
